897ca23f5fcf02eccd72a500f4720efeb4f6d5d0
wogh7572 <58222860+wogh7572@users.noreply.github.com>
2023-11-27 14:58:09 +0900

    Update 1018.py

--- python/class2/1018.py
+++ python/class2/1018.py
@@ -1,3 +1,17 @@
-#test
 n, m = map(int, input().split())
 graph = [input() for i in range(n)]
+re_m = 2500
+for i in range(n-7):
+    for j in range(m-7):
+        cnt=0
+        for y in range(8):  
+            for x in range(8):
+                b, a = i+y, j+x
+                if (b+a)%2==0:
+                    if graph[b][a]=='W':
+                        cnt+=1
+                else:
+                    if graph[b][a]=='B':
+                        cnt+=1
+        re_m = min(re_m, cnt, 64-cnt)
+print(re_m)
